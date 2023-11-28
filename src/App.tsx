c31251e30b2e3219e481c2dd03966dd95ffb8aa3
pallur <palliasgeir@gmail.com>
2023-11-28 16:57:59 +0000

    a lot of buggys

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import { motion } from "framer-motion";
 export default function App() {
   return (
     <motion.div
+      className="box"
       animate={{
-        scale: [10, 20, 20, 10, 10],
+        scale: [1, 2, 2, 1, 1],
         rotate: [0, 0, 270, 270, 0],
         borderRadius: ["20%", "20%", "50%", "50%", "20%"]
       }}
