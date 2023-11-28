a106a4477c681d2b43f072cd2e4ada332e47d9be
pallur <palliasgeir@gmail.com>
2023-11-28 23:11:15 +0000

    slowly n surely

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode, useState } from "react";
 import * as ReactDOMClient from "react-dom/client";
 import { SidebarElement } from "./Sidebar";
+import { InfoElement } from "./Info"
 import "./styles.css";
 
 import App from "./App";
@@ -16,8 +17,13 @@ function AppWithUI() {
 }
 
 const rootElement = document.getElementById("root")!;
-const sideElement = document.getElementById("sidebar")!;
 const root = ReactDOMClient.createRoot(rootElement);
+const sideElement = document.getElementById("sidebar")!;
 const sidebar = ReactDOMClient.createRoot(sideElement)
+const textElement = document.getElementById("info")!;
+const textEl = ReactDOMClient.createRoot(textElement)
+
+
 root.render(<AppWithUI />);
 sidebar.render(<SidebarElement />);
+textEl.render(<InfoElement />);
